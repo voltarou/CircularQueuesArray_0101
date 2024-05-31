526cde0bcee79a1108f711efd2fd6555cb038803
voltarou <bagusadnan04@gmail.com>
2024-05-31 11:45:03 +0800

    void display

--- CINABGSDDDD/CINABGSDDDD.cpp
+++ CINABGSDDDD/CINABGSDDDD.cpp
@@ -55,6 +55,15 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+
+    void display() {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+        }
+    }
 };
  
 int main()

cd943a2dfcb6bcee025360a2f50a96409b7af0ce
voltarou <bagusadnan04@gmail.com>
2024-05-31 11:43:18 +0800

    if else 2

--- CINABGSDDDD/CINABGSDDDD.cpp
+++ CINABGSDDDD/CINABGSDDDD.cpp
@@ -43,6 +43,17 @@ public:
             return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 };
  

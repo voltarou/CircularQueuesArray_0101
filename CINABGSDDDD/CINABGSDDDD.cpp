80d1655ebb8d57350f93eb365dab5e6f16add4aa
voltarou <bagusadnan04@gmail.com>
2024-05-31 11:57:06 +0800

    if else 3

--- CINABGSDDDD/CINABGSDDDD.cpp
+++ CINABGSDDDD/CINABGSDDDD.cpp
@@ -62,6 +62,29 @@ public:
 
         if (FRONT == -1) {
             cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        else {
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            while (FRONT_position <= REAR_position) {
+
+            }
         }
     }
 };

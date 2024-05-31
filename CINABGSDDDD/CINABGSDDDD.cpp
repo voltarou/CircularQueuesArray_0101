ef3e25b6abdd3eeb23075288b0e731558a3f9880
voltarou <bagusadnan04@gmail.com>
2024-05-31 11:41:15 +0800

    cout element deleted

--- CINABGSDDDD/CINABGSDDDD.cpp
+++ CINABGSDDDD/CINABGSDDDD.cpp
@@ -42,6 +42,7 @@ public:
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
     }
 };
  
